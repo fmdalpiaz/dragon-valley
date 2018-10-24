ba847723adcc62b77889109c7999f34a90daecb2
Fernando M. Dalpiaz <fmdalpiaz@gmail.com>
2018-10-24 14:51:47 -0300

    [wip] Working on ListDragons Component

--- src/app/dragons/list-dragons/list-dragons.component.ts
+++ src/app/dragons/list-dragons/list-dragons.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DragonsService } from './../dragons.service';
+import { Dragon } from './../dragon';
+
 @Component({
     selector: 'app-list-dragons',
     templateUrl: './list-dragons.component.html',
     styles: []
 })
 export class ListDragonsComponent implements OnInit {
+    dragons: Dragon[];
 
-    constructor() { }
+    constructor(private service: DragonsService) { }
 
     ngOnInit() {
+        this.dragons = [];
+
+        this.service
+            .getAll()
+            .subscribe(
+                data => {
+                    this.dragons = this.filterSluggedDragons(data);
+                },
+                err => console.error(err)
+            );
+    }
+
+    private filterSluggedDragons(dragons: Dragon[]): Dragon[] {
+        return dragons.filter(dragon => dragon.slug.trim() !== '' && dragon.type.trim() !== '');
     }
 
 }
